73d84d10a33b13ca7ad6fae33096a9d0aaec67f4
makleriux <makleriux@users.noreply.github.com>
2023-05-11 13:41:41 +0100

    Update Quickvision_API_test.py

--- Quickvision API test/Quickvision_API_test.py
+++ Quickvision API test/Quickvision_API_test.py
@@ -2,25 +2,23 @@
 import subprocess # lets execute programs
 import winreg as wrg #find Quicvision location for API 
 import time # Giggless
-import sys; 
+import sys 
 
-#os_max_bit = sys.maxsize > 2**32 # check if its x64 or x32
-# if (os_max_bit) is True {
-#addr = "SOFTWARE\\WOW6432Node\\Owandy\\QuickVision\\" 
-#ostyp = "x64"
-#}
-#else
-#ostyp = "x32"
-#addr = "SOFTWARE\\Owandy\\QuickVision\\"
+os_max_bit = sys.maxsize > 2**32 # check if its x64 or x32
 
+if os_max_bit is True:
+    addr = "SOFTWARE\\WOW6432Node\\Owandy\\QuickVision\\" 
+    ostyp = "x64"
+    
+else: 
+    ostyp = "x32"
+    addr = "SOFTWARE\\Owandy\\QuickVision\\"
+    
 # Store location of HKEY_CURRENT_USER
 location = wrg.HKEY_LOCAL_MACHINE\
   
 # Storing path within HKEY
-soft = wrg.OpenKeyEx(location,raddr)
-  
-#for long therm testing consider to use TRY as QV is 32bit and on ^$bit machines 6432 node is added to the reg path
-# try would find correct path or prompt QV is not installed/reg pointers not found :)
+soft = wrg.OpenKeyEx(location,r""+addr)
 
 #finding path for API
 adresas = wrg.QueryValueEx(soft,"MjExec") #tulip 0 is address
@@ -31,7 +29,7 @@ if soft:
   
 # Printing values
 print("OS type is: " + ostyp)
-print("Registry pointers for API location: "+ location+soft)
+print("Registry pointers for API location: ",r""+addr)
 hm = "QuickVision instalation location is: "
 print(hm + adresas[0])
 print("Opening QuickVision in 5 seconds")
